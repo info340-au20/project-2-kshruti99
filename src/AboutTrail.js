0a1fb66f98827babb0500b31d986eee5c87ece0f
mmk42 <52052848+mmk42@users.noreply.github.com>
2020-12-06 15:30:40 -0800

    more information button redirects

--- src/AboutTrail.js
+++ src/AboutTrail.js
@@ -5,6 +5,12 @@ import { useParams } from 'react-router-dom';
 
 function AboutTrail(props) {
 
+  //let trailName = ''; //REPLACE THIS WITH CORRECT VALUE
+  //const urlParams = useParams();
+  //trailName = urlParams.trailname;
+
+  //find the trail from the json file for trail information
+
   return (
     <div>
       <h2>Wow it's the trail info page.</h2>
