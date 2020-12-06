ad2f9f51bed5c1ac7eb311383c32659cb3b05185
mmk42 <52052848+mmk42@users.noreply.github.com>
2020-12-06 15:50:59 -0800

    issues rendering second page

--- src/AboutTrail.js
+++ src/AboutTrail.js
@@ -5,15 +5,21 @@ import { useParams } from 'react-router-dom';
 
 function AboutTrail(props) {
 
-  //let trailName = ''; //REPLACE THIS WITH CORRECT VALUE
-  //const urlParams = useParams();
-  //trailName = urlParams.trailname;
+  let ourTrail = ''; //REPLACE THIS WITH CORRECT VALUE
+  const urlParams = useParams();
+  ourTrail = urlParams.trailName;
+  console.log(ourTrail);
+
 
   //find the trail from the json file for trail information
 
+  //find trailInfo from thei
+
   return (
     <div>
-      <h2>Wow it's the trail info page.</h2>
+
+      <h2>Wow it's the trail info page for {ourTrail}.</h2>
+
     </div>
   );
 }
