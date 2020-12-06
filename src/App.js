eceef5482be713cc43126a098e95b39769452abb
kshruti99 <shrutikompella@gmail.com>
2020-12-06 01:48:02 -0800

    trail cards displaying but with bad style

--- src/App.js
+++ src/App.js
@@ -10,10 +10,13 @@ import TRAIL_INFO from './data/trail_info.csv'; //a sample list of dogs (model)
 
 function App(props) {
 
-  const trail_info = TRAIL_INFO;
-
+  //const trail_info = TRAIL_INFO;
   //const renderTrailList = (renderProps) => <RenderTrailList {...renderProps} trail_info={trail_info} />
-
+  /*
+  let allTrails = [];
+  for (var trail in props.info) {
+    allTrails.push(props.info[trail].trailName);
+  }*/
 
   return (
     <div>
@@ -41,6 +44,7 @@ function App(props) {
 
           </div>
         </div>
+        <TrailList trails={props.info}/>
       </main>
 
 
@@ -64,6 +68,38 @@ function AboutNav() {
   );
 }
 
+export function TrailCard(props) {
+  let imgSrc = 'img/'+props.trail.image;
+  let imgAlt = props.trail.trailName + " image";
+  return (
+    <div className="d-flex p-2 col-lg-4">
+      <div className="card mx-2 my-4">
+        <img className="card-img-top" src={imgSrc} alt={imgAlt}/>
+        <div className="card-body">
+          <h4 className="card-title">{props.trail.trailName}</h4>
+          <p className="card-text">{props.trail.address}</p>
+          <button className="button float-right" class="btn btn-outline-secondary"><a href="expandedview.html">See more</a></button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function TrailList(props) {
+  let deck = props.trails.map((trail) => {
+    let trailCard = <TrailCard trail={trail} />;
+    return trailCard;
+  });
+
+  return (
+    <div id="trailList" className="col-9">
+      <h2>Trails to Visit in Greater Seattle</h2>
+      <div class="card-deck">
+        {deck}
+      </div>
+    </div>
+  );
+}
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import{ BrowserRouter } from 'react-router-dom';
+import TRAIL_DATA from './data/trail_data.json';
+import TRAIL_INFO from './data/trail_info.json';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App info={TRAIL_INFO} data={TRAIL_DATA} /></BrowserRouter>, document.getElementById('root'));
