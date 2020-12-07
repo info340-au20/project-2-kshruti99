1a1ec8a0d5a88b35f7c2b2ca0f41c38fe262c8a8
mmk42 <52052848+mmk42@users.noreply.github.com>
2020-12-06 18:03:54 -0800

    ourTrail not displaying

--- src/AboutTrail.js
+++ src/AboutTrail.js
@@ -5,12 +5,14 @@ import { useParams } from 'react-router-dom';
 
 function AboutTrail(props) {
 
-  //let ourTrailName = ''; //REPLACE THIS WITH CORRECT VALUE
-  
+  let ourTrailName = ''; //REPLACE THIS WITH CORRECT VALUE
   const urlParams = useParams();
+  ourTrailName = urlParams.trailname;
 
+  let ourTrail =  _.find(props.info, {trailName: ourTrailName});
   
   console.log("ourTrail is: " + urlParams.trailname);
+  console.log("information is: " + ourTrail.zipcode);
   
 
 
