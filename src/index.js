8813db7bf14784e0cbd4a289225f118b8dd6251b
kshruti99 <shrutikompella@gmail.com>
2020-12-08 00:23:10 -0800

    firebase config

--- src/index.js
+++ src/index.js
@@ -5,5 +5,17 @@ import App from './App';
 import './index.css';
 import TRAIL_DATA from './data/trail_data.json';
 import TRAIL_INFO from './data/trail_info.json';
+import firebase from 'firebase/app';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBU3ipk7MteDrfWHvJKn2TKl-zKYmD3x34",
+    authDomain: "traillists.firebaseapp.com",
+    projectId: "traillists",
+    storageBucket: "traillists.appspot.com",
+    messagingSenderId: "678958262386",
+    appId: "1:678958262386:web:397385e537011771063aeb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<BrowserRouter><App info={TRAIL_INFO} data={TRAIL_DATA} /></BrowserRouter>, document.getElementById('root'));
